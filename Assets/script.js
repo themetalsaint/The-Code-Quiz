564f3383551fcc6e7477ecb4f023d2b7c72276db
Rachael Davis <80214032+themetalsaint@users.noreply.github.com>
2021-04-21 20:24:57 -0400

    html and basic css to help visualize

---
+++ Assets/script.js
@@ -0,0 +1,27 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//**Code For Sound */
+//const correctSound = new Audio("../sound/symphonic-slam-sound-effect.mp3");
+//const incorrectSound = new Audio("../sound/punch-sound-effect.mp3"); 
