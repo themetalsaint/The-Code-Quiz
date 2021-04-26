2ad2ecc65cd41ee46b1efa20e4c2e3062cec0752
Rachael Davis <80214032+themetalsaint@users.noreply.github.com>
2021-04-26 19:52:17 -0400

    created README and updated some of the structure work

--- Assets/script.js
+++ Assets/script.js
@@ -77,8 +77,10 @@ var startBtn = document.getElementById("start");
 var end = document.getElementById("endScreen");
 var endButtons = document.getElementById("endButtons");
 var answers = document.getElementById("answers");
+var feedbackEl = document.querySelector("#feedback");
+// var time = question.length * 12.5;
 var score = 0;
-var startingTime = 76;
+// var startingTime = 76;
 var timePenalty = 10;
 var questionIndex = 0;
 
@@ -90,34 +92,87 @@ function startGame(){
     start.classList.add("hidden");
     quizPage.classList.add("reveal")
     
-    
-    
-    
     //start timer in here 
-    //show starting time
+    // timerID = setInterval(tick, 1000);
+
+    // //show starting time
+    // timeEl.textContent = time;
+    
+    
     //call function (need new function see below) that shows questions
     showQuestions();
 
 
 }
 
+function tick(){
+    //update timer
+    time--;
+    timeEl.textContent = time;
+
+//     if(time<=0){
+//         quizEnd();
+//     }
+}
+
 function showQuestions(){
     var activeQuestion = questions[questionIndex];
+
     var triviaQuestion = document.getElementById("trivia");
+
     triviaQuestion.textContent = activeQuestion.question;
+
     answers.innerHTML = ""; //clears out old options
+
 
     activeQuestion.options.forEach(function(option, i){
         var optionBtns = document.createElement("button");
         optionBtns.setAttribute("value", option);
         // optionsBtns.setAttribute("class", "btn") btn class is for styling
         optionBtns.textContent = i + 1 + ". " + option;
-        //optionsBtns.onclick = next funtion;
+        optionBtns.onclick = clickQuestions();
 
 
         answers.appendChild(optionBtns);
 
-    })
+            }
+        )
+    }
+
+function clickQuestions(){
+ // check if user guessed wrong
+ if (this.value !== questions[questionIndex].answer) {
+    // penalize time
+    time -= 10;
+
+    if (time < 0) {
+      time = 0;
+    }
+
+    // display new time on page
+    timeEl.textContent = time;
+
+
+    feedbackEl.textContent = "Wrong!";
+  } else {
+
+    feedbackEl.textContent = "Correct!";
+  }
+
+ // flash right/wrong feedback on page 
+     feedbackEl.setAttribute("class", "feedback");
+    setTimeout(function() {
+    feedbackEl.setAttribute("class", "feedback hide");
+    }, 1000);
+
+    //Move on to next question
+    questionIndex++;
+    //Out of questions
+    // if(questionIndex === questions.length) {
+    //     quizEnd();
+    // } else {
+    //     showQuestions();
+    //     }
     }
 
 
@@ -126,9 +181,17 @@ function showQuestions(){
 
 
 
-
-
 // replayGame()
+
+
+
+
+
+
+
+
+
+// checkAnswers()
 
 
 
@@ -155,12 +218,9 @@ function showQuestions(){
 
 
 
-// checkAnswers()
+// quizEnd()
 
 
 
 
 startBtn.onclick = startGame;
-
-
-
---
+++ README.md
@@ -0,0 +1,13 @@
+
+The Code Quiz - Homework 4
+
+
+My Task:
+"At some point in your journey to become a full-stack web developer, you’ll likely be asked to complete a coding assessment—perhaps as part of an interview process. A typical coding assessment includes both multiple-choice questions and interactive coding challenges.
+
+To help familiarize you with these tests and allow you to use the skills covered in this unit, this week’s homework invites you to build a timed coding quiz with multiple-choice questions. This app will run in the browser and will feature dynamically updated HTML and CSS powered by JavaScript code that you write. It will have a clean, polished, and responsive user interface."
+
+
+This project felt liek teh first really hard challenge for me. I am struggling with functions and am struggling to finish this homework. I hope that I can show case a minor amount of skill and that I can come back to this one in the hopes of improving upin it as time goes on. 
+
+
